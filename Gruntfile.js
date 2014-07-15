dc2b653257180472bfc43e03cf95ed7d8cf57a9d
Chyld Medford <chyld.medford@gmail.com>
2014-07-15 16:37:58 -0500

    added test dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,14 @@ module.exports = function(grunt){
     // ---------------------------------------------------------------------- //
     watch: {
       code: {
-        files: ['Gruntfile.js', 'app/**/*.js'],
+        files: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js'],
         tasks: ['jshint:all']
       }
     },
     // ---------------------------------------------------------------------- //
     jshint: {
       options: {jshintrc: '.jshintrc', reporter: require('jshint-stylish')},
-      all: ['Gruntfile.js', 'app/**/*.js']
+      all: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js']
     }
     // ---------------------------------------------------------------------- //
   });
